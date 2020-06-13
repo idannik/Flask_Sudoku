f90c4f0fe991d3312700f0ed5af1805fb2d3ce2a
Idan Nikritin <Idan.nik@gmail.com>
2020-06-13 09:14:11 +0300

    refactor board html convert input to divs

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ from flask import render_template
 from flask_socketio import SocketIO, emit
 
 from data.sudopy import Sudoku
-from sudoko_solver import SudokoBacktrackSolver, SudokoSmartSolver
+from sudoko_solver import SudokoSmartSolver
 
 # data.config.from_object('local_config')
 
 app = Flask(__name__)
 
 socketio = SocketIO(app)
-
 
 if 'HEROKU' in os.environ:
     app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
@@ -25,12 +24,12 @@ if 'HEROKU' in os.environ:
 def index():
     return render_template('index.html', board=Sudoku().board)
 
+
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
     b = Sudoku().board
     emit('update_board', {'board': b, 'id': 0})
-    emit('update_board', {'board': b, 'id': 1})
 
 
 @socketio.on('my_event')
@@ -38,9 +37,11 @@ def handle_my_custom_event(json):
     emit('my_response',
          {'new_board': Sudoku().board})
 
+
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
+
 
 @socketio.on('solve')
 def handle_message(message):
@@ -58,6 +59,7 @@ def handle_message(message):
 @app.route('/solution', methods=['POST'])
 def solution():
     print('hi')
+
 
 def clean_puzzle(puzzle):
     """
--- static/js/board.js
+++ static/js/board.js
@@ -3,20 +3,23 @@ $(document).ready(function () {
     console.log('here')
 
 
+    function query_soduko_cell(id, square, cell) {
+        return document.querySelector(`#sudoku${id} > div:nth-child(${square}) > div:nth-child(${cell})`);
+    }
 
     const get_cell_accroding_to_row_and_col = (row, col, id) => {
         const square = Math.floor(row / 3) * 3 + Math.floor(col / 3) + 1
         const cell = (row % 3 )* 3 + col % 3 + 1
-        return document.querySelector(`#sudoku${id} > div:nth-child(${square}) > input:nth-child(${cell})`)
+        return query_soduko_cell(id, square, cell)
     }
 
     $('#solve-btn').click(function () {
         let board = []
-        for (let id=0; id<2; id++) {
+        for (let id=0; id<1; id++) {
             for (let i = 0; i < 9; i++) {
                 let row = []
                 for (let j = 0; j < 9; j++) {
-                    let val = get_cell_accroding_to_row_and_col(i, j, id).value
+                    let val = get_cell_accroding_to_row_and_col(i, j, id).textContent
                     if (val=='') {
                         row[j] = 0
                     } else {
@@ -40,12 +43,14 @@ $(document).ready(function () {
         for (let i = 0; i < 9; i++) {
             for (let j = 0; j < 9; j++) {
                 let div = get_cell_accroding_to_row_and_col(i, j, id)
-                if (board[i][j]>0) {
-                    div.value = board[i][j]
-                    div.disabled = true
-                } else {
-                    div.value = ''
-                    div.disabled = false
+                if (div) {
+                    if (board[i][j] > 0) {
+                        div.textContent = board[i][j]
+                        div.disabled = true
+                    } else {
+                        div.textContent = ""
+                        div.disabled = false
+                    }
                 }
 
             }
